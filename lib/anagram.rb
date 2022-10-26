c2621f083a005744441e5fcfe2692074030e5e69
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-10-26 18:44:58 +0300

    complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match arr
+        matching = []
+        arr.each do |word|
+            if word.chars.sort == @word.chars.sort
+                matching << word
+            end
+        end
+        matching
+    end
+
+end
